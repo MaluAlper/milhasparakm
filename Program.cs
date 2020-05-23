f1e22d55e4843bd32b1634c9b12ecd2a57f31508
Maria Luiza <malualper@hotmail.com>
2020-05-22 23:50:44 -0300

    milhasparakm

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace milhasparakm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string primeirovalor;
+            int milhas;
+            int km=1609;
+            int total;
+
+            Console.Write("Digite o valor da milha:");
+            primeirovalor=Console.ReadLine();
+            milhas = Int32.Parse (primeirovalor);
+            km=1609;
+            total=milhas*km;
+            Console.WriteLine("o valor de km é " + total+"km");
+            
+        }
+    }
+}
